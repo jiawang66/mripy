9aff91e54a1eb3b2dfb6e74904a2f6bbde742267
jiawong123 <jiawongms@outlook.com>
2022-07-27 20:34:18 +0800

    conv filt adjoint for 'full, valid and same' mode

--- mripy/signal/conv.py
+++ mripy/signal/conv.py
@@ -8,301 +8,94 @@ import scipy.signal as signal
 from . import config, backend, util
 
 
-def convolve(data, filt, mode='full', strides=None, multi_channel=False):
-    r"""
-    Convolution that supports multi-dimensional and multi-channel inputs.
-    Based on scipy.signal.convolve and cudnn.
+def convolve(data, filt, mode='full'):
+    """
+    Convolution
 
     Parameters
     ----------
-    data : ndarray
-        data array of shape:
-        :math:`[..., m_1, ..., m_D]` if multi_channel is False,
-        :math:`[..., c_i, m_1, ..., m_D]` otherwise.
-    filt : ndarray
-        filter array of shape:
-        :math:`[n_1, ..., n_D]` if multi_channel is False
-        :math:`[c_o, c_i, n_1, ..., n_D]` otherwise.
-    mode : str {'full', 'same', 'valid'}, optional
-
-        ``full``
-           The output is the full discrete linear convolution
-           of the inputs. (Default)
-        ``valid``
-           The output consists only of those elements that do not
-           rely on the zero-padding. In 'valid' mode, either `in1` or `in2`
-           must be at least as large as the other in every dimension.
-        ``same``
-           The output is the same size as `in1`, centered
-           with respect to the 'full' output.
-    strides : None or list or tuple of ints
-        convolution strides of length D.
-    multi_channel : bool
-        specify if input/output has multiple channels.
+    data
+    filt
+    mode
 
     Returns
     -------
-    output : ndarray
-        output array of shape:
-        :math:`[..., p_1, ..., p_D]` if multi_channel is False,
-        :math:`[..., c_o, p_1, ..., p_D]` otherwise.
 
-    See Also
-    --------
-    scipy.signal.convolve
     """
-    xp = backend.get_array_module(data)
-    if xp == np:
-        output = _convolve(data, filt, mode=mode, strides=strides,
-                           multi_channel=multi_channel)
-    else:
-        raise NotImplementedError('Currently not support convolution for cupy.')
-
-    return output
+    pass
 
 
-def convolve_data_adjoint(output, filt, data_shape,
-                          mode='full', strides=None, multi_channel=False):
-    r"""
-    Adjoint convolution operation with respect to data.
+def convolve_data_adjoint(output, filt, data_shape, mode='full'):
+    """
+    Adjoint of convolution with respect to data
 
     Parameters
     ----------
     output : ndarray
-        An array of the resolution of conv(data, filt).
-        output array of shape
-        :math:`[..., p_1, ..., p_D]` if multi_channel is False,
-        :math:`[..., c_o, p_1, ..., p_D]` otherwise.
     filt : ndarray
-        filt array of shape
-        :math:`[n_1, ..., n_D]` if multi_channel is False
-        :math:`[c_o, c_i, n_1, ..., n_D]` otherwise.
-    data_shape : list or tuple
-        the shape of data of length D
-    mode : str {'full', 'same', 'valid'}, optional
-    strides : None or list or tuple of ints
-        convolution strides of length D.
-    multi_channel : bool
-        specify if input/output has multiple channels.
+    data_shape : int or list or tuple
+    mode: str {'full', 'valid', 'same'}, optional
 
     Returns
     -------
-    data : ndarray
-        data array of shape:
-        :math:`[..., m_1, ..., m_D]` if multi_channel is False,
-        :math:`[..., c_i, m_1, ..., m_D]` otherwise.
+    ndarray
     """
-    data_shape = tuple(data_shape)
+    filt_shape = tuple(k if k % 2 == 1 else k + 1 for k in filt.shape)
+    filt = util.resize(filt, filt_shape)
+    filt = _reverse_and_conj(filt)
 
-    xp = backend.get_array_module(output)
-    if xp == np:
-        data = _convolve_data_adjoint(output, filt, data_shape,
-                                      mode=mode, strides=strides,
-                                      multi_channel=multi_channel)
+    if mode == 'full' or mode == 'valid':
+        adjoint_mode = 'valid'
+
+        # zero pad the output
+        pad_shape = [i + j - 1 for i, j in zip(data_shape, filt_shape)]
+        num_left = [int(np.ceil((i - j) / 2)) for i, j in zip(pad_shape, output.shape)]
+        num_right = [(i - j) // 2 for i, j in zip(pad_shape, output.shape)]
+        dims = list(range(output.ndim))
+        output = util.crop_pad(output, dims=dims, num_left=num_left, num_right=num_right)
+
+    elif mode == 'same':
+        adjoint_mode = 'same'
     else:
-        raise NotImplementedError('Currently not support convolution adjoint for cupy.')
+        raise ValueError(f'Unknown mode, got {mode}')
 
+    data = signal.convolve(output, filt, mode=adjoint_mode)
     return data
 
 
-def convolve_filter_adjoint(output, data, filt_shape,
-                            mode='full', strides=None, multi_channel=False):
-    r"""
-    Adjoint convolution operation with respect to filter.
+def convolve_filt_adjoint(output, data, filt_shape, mode='full'):
+    """
+    Adjoint of convolution with respect to filter
 
     Parameters
     ----------
     output : ndarray
-        An array of the resolution of conv(data, filt).
-        output array of shape
-        :math:`[..., p_1, ..., p_D]` if multi_channel is False,
-        :math:`[..., c_o, p_1, ..., p_D]` otherwise.
     data : ndarray
-        data array of shape:
-        :math:`[..., m_1, ..., m_D]` if multi_channel is False,
-        :math:`[..., c_i, m_1, ..., m_D]` otherwise.
-    filt_shape : list or tuple
-        the shape of filter of length D
-    mode : str {'full', 'same', 'valid'}, optional
-    strides : None or list or tuple of ints
-        convolution strides of length D.
-    multi_channel : bool
-        specify if input/output has multiple channels.
+    filt_shape : int or list or tuple
+    mode: str {'full', 'valid', 'same'}, optional
 
-    Returns:
-    filt : ndarray
-        filtlter array of shape
-        :math:`[n_1, ..., n_D]` if multi_channel is False
-        :math:`[c_o, c_i, n_1, ..., n_D]` otherwise.
+    Returns
+    -------
+    ndarray
     """
-    filt_shape = tuple(filt_shape)
-    xp = backend.get_array_module(data)
-    if xp == np:
-        filt = _convolve_filter_adjoint(output, data, filt_shape,
-                                        mode=mode, strides=strides,
-                                        multi_channel=multi_channel)
-    else:  # pragma: no cover
-        raise NotImplementedError('Currently not support convolution adjoint for cupy.')
-
-    return filt
-
-
-def _convolve(data, filt, mode='full', strides=None, multi_channel=False):
-    D, b, B, m, n, s, c_i, c_o, p = _get_convolve_params(data.shape, filt.shape,
-                                                         mode, strides, multi_channel)
-
-    # Normalize shapes.
-    data = data.reshape((B, c_i) + m)
-    filt = filt.reshape((c_o, c_i) + n)
-    output = np.zeros((B, c_o) + p, dtype=data.dtype)
-    slc = tuple(slice(None, None, s_d) for s_d in s)
-
-    # Why multi-channel need c_i and c_o?
-    for k in range(B):
-        for j in range(c_o):
-            for i in range(c_i):
-                # why self add?
-                output[k, j] += signal.convolve(
-                    data[k, i], filt[j, i], mode=mode)[slc]
-
-    # Reshape.
-    if multi_channel:
-        output = output.reshape(b + (c_o, ) + p)
-    else:
-        output = output.reshape(b + p)
-
-    return output
-
-
-def _convolve_data_adjoint(output, filt, data_shape,
-                           mode='full', strides=None,
-                           multi_channel=False):
-    D, b, B, m, n, s, c_i, c_o, p = _get_convolve_params(data_shape, filt.shape,
-                                                         mode, strides, multi_channel)
-
-    # Normalize shapes.
-    output = output.reshape((B, c_o) + p)
-    filt = filt.reshape((c_o, c_i) + n)
-    data = np.zeros((B, c_i) + m, dtype=output.dtype)
-    slc = tuple(slice(None, None, s_d) for s_d in s)
-    adjoint_mode = None
-    output_kj = None
-
-    if mode == 'full':
-        output_kj = np.zeros([m_d + n_d - 1 for m_d, n_d in zip(m, n)], dtype=output.dtype)
-        adjoint_mode = 'valid'
-    elif mode == 'valid':
-        output_kj = np.zeros([max(m_d, n_d) - min(m_d, n_d) + 1 for m_d, n_d in zip(m, n)],
-                             dtype=output.dtype)
-        if all(m_d >= n_d for m_d, n_d in zip(m, n)):
-            adjoint_mode = 'full'
-        else:
-            adjoint_mode = 'valid'
+    if mode == 'full' or mode == 'valid':
+        return convolve_data_adjoint(output, data, filt_shape, mode=mode)
     elif mode == 'same':
-        raise NotImplementedError(f'Not support for mode {mode}')
-        # output_kj = np.zeros([m_d for m_d, n_d in zip(m, n)], dtype=output.dtype)
-        # adjoint_mode = 'same'
-
-    for k in range(B):
-        for j in range(c_o):
-            for i in range(c_i):
-                output_kj[slc] = output[k, j]
-                data[k, i] += signal.correlate(output_kj, filt[j, i], mode=adjoint_mode)
-
-    # Reshape.
-    data = data.reshape(data_shape)
-    return data
-
-
-def _convolve_filter_adjoint(output, data, filt_shape,
-                             mode='full', strides=None,
-                             multi_channel=False):
-    D, b, B, m, n, s, c_i, c_o, p = _get_convolve_params(data.shape, filt_shape,
-                                                         mode, strides, multi_channel)
-
-    # Normalize shapes.
-    data = data.reshape((B, c_i) + m)
-    output = output.reshape((B, c_o) + p)
-    slc = tuple(slice(None, None, s_d) for s_d in s)
-    adjoint_mode = None
-    output_kj = None
-
-    if mode == 'full':
-        output_kj = np.zeros([m_d + n_d - 1 for m_d, n_d in zip(m, n)],
-                             dtype=output.dtype)
         adjoint_mode = 'valid'
-    elif mode == 'valid':
-        output_kj = np.zeros([max(m_d, n_d) - min(m_d, n_d) + 1 for m_d, n_d in zip(m, n)],
-                             dtype=output.dtype)
-        if all(m_d >= n_d for m_d, n_d in zip(m, n)):
-            adjoint_mode = 'valid'
-        else:
-            adjoint_mode = 'full'
-    elif mode == 'same':
-        raise NotImplementedError(f'Not support for mode {mode}')
-        # output_kj = np.zeros([m_d for m_d, n_d in zip(m, n)], dtype=output.dtype)
-        # adjoint_mode = 'same'
+        flag = [True if i % 2 == 1 and j % 2 == 0 else False
+                for i, j in zip(data.shape, filt_shape)]
+        output_shape = [i + j - 1if f else i
+                        for i, j, f in zip(data.shape, filt_shape, flag)]
+        output = util.resize(output, output_shape)
 
-    filt = np.zeros((c_o, c_i) + n, dtype=output.dtype)
-    for k in range(B):
-        for j in range(c_o):
-            for i in range(c_i):
-                output_kj[slc] = output[k, j]
-                filt[j, i] += signal.correlate(output_kj, data[k, i], mode=adjoint_mode)
-
-    # Reshape.
-    filt = filt.reshape(filt_shape)
-    return filt
-
-
-def _get_convolve_params(data_shape, filt_shape,
-                         mode, strides, multi_channel):
-    D = len(filt_shape) - 2 * multi_channel     # ndim of data or filt without multi channel
-    m = tuple(data_shape[-D:])                  # data shape without multi channel
-    n = tuple(filt_shape[-D:])                  # filt shape without multi channel
-    b = tuple(data_shape[:-D - multi_channel])  # what is the role of b? b is a always empty tuple ().
-    B = util.prod(b)                            # B = 1 ?
-
-    if multi_channel:
-        if filt_shape[-D - 1] != data_shape[-D - 1]:
-            raise ValueError(f'Data channel mismatch, '
-                             f'got {data_shape[-D - 1]} from data '
-                             f'and {filt_shape[-D - 1]} from filt.')
-
-        c_i = filt_shape[-D - 1]
-        c_o = filt_shape[-D - 2]
+        return convolve_data_adjoint(output, data, filt_shape, mode=adjoint_mode)
     else:
-        c_i = 1
-        c_o = 1
+        raise ValueError(f'Unknown mode, got {mode}')
 
-    if strides is None:
-        s = (1, ) * D
-    else:
-        if len(strides) != D:
-            raise ValueError(f'Strides must have length {D}.')
 
-        s = tuple(strides)
-
-    # determine the shape of output
-    if mode == 'full':
-        p = tuple((m_d + n_d - 1 + s_d - 1) // s_d
-                  for m_d, n_d, s_d in zip(m, n, s))
-    elif mode == 'valid':
-        if (any(m_d >= n_d for m_d, n_d in zip(m, n)) and
-            any(m_d < n_d for m_d, n_d in zip(m, n))):
-            raise ValueError('In valid mode, either data or filter must be '
-                             'at least as large as the other in every axis.')
-
-        p = tuple((m_d - n_d + 1 + s_d - 1) // s_d
-                  for m_d, n_d, s_d in zip(m, n, s))
-    elif mode =='same':
-        if (any(m_d >= n_d for m_d, n_d in zip(m, n)) and
-            any(m_d < n_d for m_d, n_d in zip(m, n))):
-            raise ValueError('In valid mode, either data or filter must be '
-                             'at least as large as the other in every axis.')
-        p = tuple((m_d + s_d - 1) // s_d
-                  for m_d, n_d, s_d in zip(m, n, s))
-    else:
-        raise ValueError('Invalid mode, got {}'.format(mode))
-
-    return D, b, B, m, n, s, c_i, c_o, p
+def _reverse_and_conj(x):
+    """
+    Reverse array `x` in all dimensions and perform the complex conjugate
+    """
+    reverse = (slice(None, None, -1),) * x.ndim
+    return x[reverse].conj()
--- test/test.py
+++ test/test.py
@@ -3,78 +3,17 @@ Only for testing some ideas
 """
 
 import numpy as np
+import scipy.signal as signal
 import mripy.signal as sig
-from scipy.signal import convolve, correlate
 
 
-def _reverse_and_conj(x):
-    """
-    Reverse array `x` in all dimensions and perform the complex conjugate
-    """
-    reverse = (slice(None, None, -1),) * x.ndim
-    return x[reverse].conj()
-
-
-def convolve_data_adjoint(output, filt, data_shape, mode='full'):
-    """
-    Adjoint of convolution with respect to data
-
-    Parameters
-    ----------
-    output : ndarray
-    filt : ndarray
-    data_shape : int or list or tuple
-    mode: str {'full', 'valid', 'same'}, optional
-
-    Returns
-    -------
-    ndarray
-    """
-    filt_shape = tuple(k if k % 2 == 1 else k + 1 for k in filt.shape)
-    filt = sig.util.resize(filt, filt_shape)
-    filt = _reverse_and_conj(filt)
-
-    if mode == 'full' or mode == 'valid':
-        adjoint_mode = 'valid'
-
-        # zero pad the output
-        pad_shape = [i + j - 1 for i, j in zip(data_shape, filt_shape)]
-        num_left = [int(np.ceil((i - j) / 2)) for i, j in zip(pad_shape, output.shape)]
-        num_right = [(i - j) // 2 for i, j in zip(pad_shape, output.shape)]
-        dims = list(range(output.ndim))
-        output = sig.util.crop_pad(output, dims=dims, num_left=num_left, num_right=num_right)
-
-    elif mode == 'same':
-        adjoint_mode = 'same'
-    else:
-        raise ValueError(f'Unknown mode, got {mode}')
-
-    data = convolve(output, filt, mode=adjoint_mode)
-    return data
-
-
-def convolve_filt_adjoint(output, data, filt_shape, mode='full'):
-    """
-    Adjoint of convolution with respect to filter
-
-    Parameters
-    ----------
-    output : ndarray
-    data : ndarray
-    filt_shape : int or list or tuple
-    mode: str {'full', 'valid', 'same'}, optional
-
-    Returns
-    -------
-    ndarray
-    """
-    return convolve_data_adjoint(output, data, filt_shape, mode=mode)
+EPS = 1e-16
 
 
 def error(x0, y0, x1, y1):
     inner_1 = np.dot(vec(y0), vec(y1))
     inner_2 = np.dot(vec(x0), vec(x1))
-    e12 = np.abs(inner_1 - inner_2) / np.abs(inner_1)
+    e12 = np.abs(inner_1 - inner_2) / np.abs(inner_1 + EPS)
     print(f'error = {e12:.4e}')
 
     assert e12 < 1e-12
@@ -84,30 +23,43 @@ def vec(arr):
     return arr.reshape(-1)
 
 
-def main():
-    xshape = (1, 6, 7, 8)
-    dshape = (8, 7, 1, 6)
-    # xshape = (2, 3, 3, 2)
-    # dshape = (5, 6, 7, 8)
-    mode = 'same'
-
+def run_test(xshape, dshape, mode):
     # x0 = np.random.randn(*xshape)
     # d0 = np.random.randn(*dshape)
     x0 = np.array(range(np.prod(xshape))).reshape(xshape)
     d0 = np.array(range(np.prod(dshape))).reshape(dshape)
 
     # convolution
-    y0 = convolve(x0, d0, mode=mode)
+    y0 = signal.convolve(x0, d0, mode=mode)
     y1 = np.random.randn(*y0.shape)
+    # y1 = y0[::-1]
 
     # convolve_data_adjoint
-    x1 = convolve_data_adjoint(y1, d0, xshape, mode=mode)
+    x1 = sig.conv.convolve_data_adjoint(y1, d0, xshape, mode=mode)
     error(x0, y0, x1, y1)
 
     # convolve_filt_adjoint
-    d1 = convolve_filt_adjoint(y0, x0, dshape, mode=mode)
+    d1 = sig.conv.convolve_filt_adjoint(y1, x0, dshape, mode=mode)
     error(d0, y0, d1, y1)
 
+
+def main():
+
+    xshape = (2, 3, 4, 3, 1, 1, 4, 2, 3)
+    dshape = (3, 4, 2, 3, 4, 1, 1, 2, 3)
+    mode = 'full'
+    run_test(xshape, dshape, mode)
+
+    mode = 'same'
+    run_test(xshape, dshape, mode)
+
+    xshape = (1, 2, 3, 2, 3, 3, 1, 2)
+    dshape = (3, 3, 5, 4, 5, 3, 1, 2)
+    mode = 'valid'
+    run_test(xshape, dshape, mode)
+
+    xshape, dshape = dshape, xshape
+    run_test(xshape, dshape, mode)
 
 if __name__ == '__main__':
     main()
